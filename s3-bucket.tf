534952ec8931c74ea411cd895da92300c58148de
Wei Quan <yapweiquan@gmail.com>
2022-09-30 07:28:28 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/yapweiquan-dsta/s3-bucket/aws"
+  version = "2.8.0"
 
   bucket_prefix = "weiquanyap"
   acl    = "private"
